e89d8e3eeedf62ce0c0f600db09aa829641a216f
Sambeg Raj Subedi <subedisambeg111@gmail.com>
2020-09-29 16:49:56 -0400

    trying to connect database.

---
+++ Function/db.php
@@ -0,0 +1,38 @@
+<?php
+
+    $con = mysqli_connect('localhost', 'root', 'root', 'root');
+
+    if(!$con) {
+        echo 'Connection Error';
+    }
+
+    //Function Clean String values
+    function escape($string) {
+        global $con;
+        return mysqli_real_escape_string($con, $string);
+    }
+
+    //Query Function
+    function Query($query) {
+        global $con;
+        return mysqli_query($con, $query);
+    }
+
+    //Confirmation Function
+    function confirm($result) {
+        global $con;
+        if(!$result){
+            die('Query Failed'.mysqli_error($con));
+        }
+    }
+
+    //Fetch Data From Database
+    function fetch_data($result) {
+        return mysqli_fetch_assoc($result);
+    }
+
+    //Row Values From Database
+    function row_count($count) {
+        return mysqli_num_rows($count);
+    }
+?>
