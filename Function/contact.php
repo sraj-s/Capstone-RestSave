d28f1fbdfb4f466392d5453e47b40bbb629d9cf1
Sambeg Raj Subedi <subedisambeg111@gmail.com>
2020-09-30 16:55:01 -0400

    working on contact

--- Function/contact.php
+++ Function/contact.php
@@ -5,7 +5,7 @@ if(!$con) {
     echo 'Connection Error';
 }
 
-//mysql_select_db("test");
+
 
 $companyname =$_POST['companyname'];
 $organizationtype= $_POST['organizationtype'];
@@ -16,15 +16,11 @@ $emailaddress=$_POST['emailaddress'];
 
 
 $sql ="INSERT INTO datas (Company_Name, Organization_Type, Users_Fullname, Phone_Number, Email_Address) VALUES ('$companyname' ,'$organizationtype', '$name', '$phonenumber', '$emailaddress')";
-//$sql=mysql_query($select);
+
 $rs = mysqli_query($con, $sql);
 if($rs)
 {
 	echo "Contact Records Inserted";
 }
-//print '<script type= "text/javascript">';
-//print 'alert("The data is inserted...")';
-//print '</script>';
 
-//mysqli_close();
 ?>
